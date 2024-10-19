fb56a0c6d22956578fe455f13d5f65a81ffe5234
ZuzkaGlaess <zuzka.glaess@gmail.com>
2024-10-19 22:30:27 +0200

    fixed picture upload and display

--- src/js/profile.js
+++ src/js/profile.js
@@ -22,6 +22,7 @@ function getProfile() {
                 $("#username").val(response.username);
                 $("#email").val(response.email);
                 $("#gender").val(response.gender);
+                loadPicture(response.profilePictureRef);
             } else {
                 console.log("Error...!");
             }
@@ -33,3 +34,43 @@ function getProfile() {
 }
 
 getProfile();
+
+async function loadPicture(profilePictureRef) {
+    if(profilePictureRef != null) {
+        $.ajax({
+            url: 'http://localhost:8080/api/pictures/' + profilePictureRef,
+            type: 'GET',
+            //dataType: 'text',
+            crossDomain: true,
+            xhrFields: {
+                withCredentials: true
+            },
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem("token"),
+            },
+            //responseType: 'text',
+            processData: false,
+            async: false,
+            success: function (response, status, xhr){
+                console.log(response);
+                // console.log(atob(response));
+                // console.log(btoa(encodeURIComponent(response)));
+                // console.log(xhr.getAllResponseHeaders());
+                const type = xhr.getResponseHeader('content-type');
+                // console.log(type);
+                // const blob = new Blob([response], { type });
+                // const reader = new FileReader();
+                // reader.onload = e => {
+                //
+                // };
+                // reader.readAsDataURL(blob);
+                document.getElementById('profile_pic').setAttribute('src', "data:" + type + ";base64," + response);
+            },
+            error: function(xhr, status, error) {
+                console.error('Error downloading profile picture', error);
+            }
+        });
+    } else {
+        document.getElementById('profile_pic').setAttribute('src', "../src/images/profile_pic.png");
+    }
+}
