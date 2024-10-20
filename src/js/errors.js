d6610ab0f219098271fe5209044dd84d923a517a
ZuzkaGlaess <zuzka.glaess@gmail.com>
2024-10-20 22:01:07 +0200

    display input validation errors on pages

--- src/js/errors.js
+++ src/js/errors.js
@@ -3,19 +3,21 @@ function displayError(errorContainerID, xhr, status, error) {
     var respJson = JSON.parse(resp);
     const alertPlaceholder = document.getElementById(errorContainerID);
 
-    const errorAlert = (message, type) => {
+    const errorAlert = (message, data, type) => {
         const wrapper = document.createElement('div')
         wrapper.innerHTML = [
             `<div class="alert alert-${type} d-flex align-items-center alert-dismissible" role="alert">`,
                 `<div>${message}</div>`,
+                `<div> ${data}</div>`,
             '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
             '</div>'
         ].join('')
 
         alertPlaceholder.append(wrapper)
     }
-
-    errorAlert(respJson.message, 'danger');
+    var data =respJson.data;
+    if(Object.is(data, null)) data = "";
+    errorAlert(respJson.message, data , 'danger');
 
 }
 
